195e030568bbe660ee1ac68983a6129607e4ab01
AndomiAnsari <andomiuddeen@gmail.com>
2022-08-09 11:59:07 -0700

    1st complete tutorial

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
-
+const Goal = require('../models/goalModel')
 //@desc Get Golas
 //@route Get /api/gloas
 //@acces Private
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get Goals' })
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 //@desc Set Goal
@@ -19,8 +20,12 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('please add a text field')
     }
+    const goal = await Goal.create({
+        text: req.body.text
+    })
 
-    res.status(200).json({ message: 'set goals' })
+    // res.status(200).json({ message: 'set goals' })
+    res.status(200).json(goal)
 })
 
 
@@ -29,7 +34,15 @@ const setGoal = asyncHandler(async (req, res) => {
 //@acces Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal Not Found')
+    }
+    const UpdatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(UpdatedGoal)
 })
 
 
@@ -38,9 +51,21 @@ const updateGoal = asyncHandler(async (req, res) => {
 //@acces Private
 
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal Not Found')
+    }
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
 }
+
 )
+// const deleteGoal = asyncHandler(async (req, res) => {
+//     res.status(200).json({ message: `Delete goal ${req.params.id}` })
+// }
+// )
 
 
 
